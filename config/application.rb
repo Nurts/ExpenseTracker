04b27940e5205fa9e4642ee1c4abfaa344ea8548
Арман Тарпанов <arma23430@gmail.com>
2018-07-01 12:54:20 +0600

    gem figaro

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.paths << Rails.root.join("app", "assets", "images", "levers")
 module ExpenseTracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
